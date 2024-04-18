4576058e9c9d99502a8bcf53e05431e4b943189f
neilbluejohn <111980378+neilbluejohn@users.noreply.github.com>
2024-04-18 20:28:09 +0100

    Update tt_um_micro_mult_njp.v

--- src/tt_um_micro_mult_njp.v
+++ src/tt_um_micro_mult_njp.v
@@ -31,8 +31,8 @@ module tt_um_micro_mult_njp (
   assign B[3:0] = ui_in[7:4];
   assign uo_out[7:0] = MP[7:0];
                
-  ControlUnit (.sys_clk(sys_clk), .sys_rst(sys_rst), .flag(flag), .C0(C0), .C1(C1), .C2(C2), .C3(C3), .C4(C4), .C5(C5), .C6(C6), .C7(C7), .C8(C8), .C9(C9), .C10(C10), .C11(C11), .C12(C12));
+  ControlUnit P0 (.sys_clk(sys_clk), .sys_rst(sys_rst), .flag(flag), .C0(C0), .C1(C1), .C2(C2), .C3(C3), .C4(C4), .C5(C5), .C6(C6), .C7(C7), .C8(C8), .C9(C9), .C10(C10), .C11(C11), .C12(C12));
 
-  DataPath (.sys_clk(sys_clk), .sys_rst(sys_rst), .flag(flag), .inputA(A), .inputB(B), .SMP_out(MP), .enA(C0), .enB(C1), .enDPO(C2), .ABsel(C3), .sr_c1(C4), .sr_c0(C5), .enSR(C6), .SRsel(C7), .alu_c0(C8), .alu_c1(C9), .alu_c2(C10), .enACC(C11), .clrACC(C12));
+  DataPath P1 (.sys_clk(sys_clk), .sys_rst(sys_rst), .flag(flag), .inputA(A), .inputB(B), .SMP_out(MP), .enA(C0), .enB(C1), .enDPO(C2), .ABsel(C3), .sr_c1(C4), .sr_c0(C5), .enSR(C6), .SRsel(C7), .alu_c0(C8), .alu_c1(C9), .alu_c2(C10), .enACC(C11), .clrACC(C12));
   
 endmodule
